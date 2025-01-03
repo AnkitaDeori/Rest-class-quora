cc08827ed96b254f0eeb7da7462dcf736704526b
Ankita Deori <deoriankita2004@gmail.com>
2025-01-03 21:41:49 +0530

    Update in index.js file

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express=require("express");
 const app=express();
 const port =8080;
+const path =require("path");
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
 
 app.listen(port , () =>{
     console.log("Listening to port: 8080");
